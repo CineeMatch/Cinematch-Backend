b958c7857b9e80b4e3b70b44e4abf799ce26bd7e
Abdullah Can <abdullahcan123gs@hotmail.com>
2025-06-02 17:38:11 +0300

    comment endpoints are tested successfully

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -6,7 +6,6 @@ const authMiddleware = async (req, res, next) => {
     const token = req.header('Authorization')?.replace('Bearer ', '');
     if (!token) return res.status(401).json({ error: 'Token missing' });
 
-    console.log("token :", token);
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
     const user = await User.findOne({
