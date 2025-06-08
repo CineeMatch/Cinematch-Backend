215b0271d840875ac818f4fb98e692002447d6d3
Abdullah Can <abdullahcan123gs@hotmail.com>
2025-06-08 23:58:49 +0300

    merged with sena

--- routes/friendRoute.js
+++ routes/friendRoute.js
@@ -1,12 +1,15 @@
-import { getAllFriends, getFriendById, getUserFriends, createFriend, deleteFriend } from "../controllers/friendController.js";
+import { getAllFriends, getFriendById, getUserFriends, createFriend, deleteFriend, acceptFriendRequest, rejectFriendRequest } from "../controllers/friendController.js";
+import authMiddleware from "../middlewares/authMiddleware.js";
 import { Router } from "express";
 
 const router = Router();
 
-router.route("/friends").get(getAllFriends);
-router.route("/friend/:id").get(getFriendById);
-router.route("/friend/user/:userId").get(getUserFriends);
-router.route("/friend/create").post(createFriend);
-router.route("/friend/delete/:id").delete(deleteFriend);
+router.route("/friends").get(authMiddleware, getAllFriends);
+router.route("/friend/user").get(authMiddleware, getUserFriends);
+router.route("/friend/:id").get(authMiddleware, getFriendById);
+router.route("/friend/create").post(authMiddleware, createFriend);
+router.route("/friend/delete/:id").delete(authMiddleware, deleteFriend);
+router.route("/friend/accept").put(authMiddleware, acceptFriendRequest);
+router.route("/friend/reject").put(authMiddleware, rejectFriendRequest);
 
 export default router;
--- routes/movieTypeRoute.js
+++ routes/movieTypeRoute.js
@@ -1,22 +1,21 @@
 import { getAllMovieTypes, getFavoriteMovieTypes, getWatchedMovieTypes, getWishListMovieTypes, getMovieTypeById, createMovieType, updateMovieType, deleteMovieType } from "../controllers/movieTypeController.js";
 import { getUserMovies } from "../utils/recommendationAI.js";
+import authMiddleware from "../middlewares/authMiddleware.js";
 import { Router } from "express";
 
 const router = Router();
 
-router.route("/movieTypes").get(getAllMovieTypes);
-router.route("/movieTypes/favorites").get(getFavoriteMovieTypes);
-router.route("/movieTypes/wishlist").get(getWishListMovieTypes);
-router.route("/movieTypes/watched").get(getWatchedMovieTypes);
+router.route("/movieTypes").get(authMiddleware, getAllMovieTypes);
+router.route("/movieTypes/favorites").get(authMiddleware, getFavoriteMovieTypes);
+router.route("/movieTypes/wishlist").get(authMiddleware, getWishListMovieTypes);
+router.route("/movieTypes/watched").get(authMiddleware, getWatchedMovieTypes);
 
-router.route("/movieType/:id").get(getMovieTypeById);
-router.route("/movieType/create").post(createMovieType);
-router.route("/movieType/update/:id").put(updateMovieType);
-router.route("/movieType/delete/:id").delete(deleteMovieType);
+router.route("/movieType/:id").get(authMiddleware, getMovieTypeById);
+router.route("/movieType/create").post(authMiddleware, createMovieType);
+router.route("/movieType/update/:id").put(authMiddleware, updateMovieType);
+router.route("/movieType/delete/:id").delete(authMiddleware, deleteMovieType);
 
-//for ai
-
-router.route("user/:userId/movieTypes").get(getUserMovies);
+router.route("user/:userId/movieTypes").get(authMiddleware, getUserMovies);
 
 
 export default router;
