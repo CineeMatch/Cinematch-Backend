8e41e9f96f0e679d9f64ccd5ed08a63d0fc7a8d2
Abdullah Can <abdullahcan123gs@hotmail.com>
2025-06-10 21:12:55 +0300

    user routes are updated

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,15 +1,16 @@
 import { createUser, deleteUser, getActiveUser, getUserByID, getAllUsers, updateUser, updateActiveUser } from "../controllers/userController.js";
 import express from "express";
+import authMiddleware from "../middlewares/authMiddleware.js";
 
 const router = express.Router();
 
 router.route("/user/update/:id").put(updateUser);
 
-router.route("/users").get(getAllUsers);
-router.route("/user/:id").get(getUserByID);
-router.route("/user").get(getActiveUser);
-router.route("/user/create").post(createUser);
-router.route("/user/delete/:id").delete(deleteUser);
+router.route("/users").get(authMiddleware, getAllUsers);
+router.route("/user/:id").get(authMiddleware, getUserByID);
+router.route("/user").get(authMiddleware, getActiveUser);
+router.route("/user/create").post(authMiddleware, createUser);
+router.route("/user/delete/:id").delete(authMiddleware, deleteUser);
 
 router.route("/user/update").put(updateActiveUser);
 export default router;
