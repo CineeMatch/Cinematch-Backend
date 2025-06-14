1b474fbca1da182e3393f75dd176c38767816c30
Abdullah Can <abdullahcan123gs@hotmail.com>
2025-06-14 22:56:59 +0300

    cloudinary settings setted

---
+++ configs/cloudinary.js
@@ -0,0 +1,10 @@
+import cloudinary from 'cloudinary';
+import globalConfig from './globalConfig.js';
+
+cloudinary.config({
+    cloud_name: globalConfig.cloudinary.cloudName,
+    api_key: globalConfig.cloudinary.apiKey,
+    api_secret: globalConfig.cloudinary.apiSecret,
+});
+
+export default cloudinary;
