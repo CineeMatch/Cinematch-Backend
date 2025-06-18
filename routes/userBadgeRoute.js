4650a89b3477d98d75d30f05d966f07788b8d031
Abdullah Can <abdullahcan123gs@hotmail.com>
2025-06-18 21:27:47 +0300

    getBadgesByUserId endpoint created

--- routes/userBadgeRoute.js
+++ routes/userBadgeRoute.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { getAllUserBadges, getUserBadgeById, createUserBadge, updateUserBadge, deleteUserBadge } from '../controllers/userBadgeController.js';
+import { getAllUserBadges, getUserBadgeById, createUserBadge, updateUserBadge, deleteUserBadge, getBadgesByUserId } from '../controllers/userBadgeController.js';
 import authMiddleware from '../middlewares/authMiddleware.js';
 
 const router = express.Router();
@@ -9,5 +9,6 @@ router.route('/user-badge/:id').get(authMiddleware, getUserBadgeById);
 router.route('/user-badge/create').post(authMiddleware, createUserBadge);
 router.route('/user-badge/update/:id').put(authMiddleware, updateUserBadge);
 router.route('/user-badge/delete/:id').delete(authMiddleware, deleteUserBadge);
+router.route('/user-badge/user/:userId').get(authMiddleware, getBadgesByUserId);
 
 export default router;
