2d62b71ac369636728a66a09f7e53915ede653d9
Abdullah Can <abdullahcan123gs@hotmail.com>
2025-06-19 21:13:21 +0300

    challangeQuestion Controller, Route and Model is updated

--- models/challengeQuestion.js
+++ models/challengeQuestion.js
@@ -1,18 +1,26 @@
 import { DataTypes } from 'sequelize';
 import sequelize from '../configs/database.js';
-//sor
-const ChallangeQuestion = sequelize.define('ChallangeQuestion', {
+
+const ChallengeQuestion = sequelize.define('ChallengeQuestion', {
   id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
-  challange_id: DataTypes.INTEGER,
+  challenge_id: DataTypes.INTEGER,
   created_by: DataTypes.INTEGER,
   directed_to: DataTypes.INTEGER,
   questionText: DataTypes.TEXT,
-  correct_answer: DataTypes.STRING,
-  selected_answer: DataTypes.STRING,
-  answered_at: DataTypes.DATE,
+  correct_answer: DataTypes.BOOLEAN,
+  selected_answer: {
+    type: DataTypes.BOOLEAN,
+    allowNull: true, // This field can be null if the question hasn't been answered yet
+    defaultValue: null, // Default value is null
+  },
+  answered_at: {
+    type: DataTypes.DATE,
+    allowNull: true, // This field can be null if the question hasn't been answered yet
+    defaultValue: null, // Default value is null
+  }
 }, {
-  tableName: 'challangequestion',
+  tableName: 'challengequestion',
   timestamps: false,
 });
 
-export default ChallangeQuestion;
+export default ChallengeQuestion;
--- routes/challengeQuestionRoute.js
+++ routes/challengeQuestionRoute.js
@@ -1,13 +1,14 @@
 import express from 'express';
-import { getAllChallengeQuestions, getChallengeQuestionById, getChallengeQuestionsByUserId, createChallengeQuestion, deleteChallengeQuestion, answerChallengeQuestion } from '../controllers/challengeQuestionController.js';
+import { getAllChallengeQuestions, getChallengeQuestionById, getChallengeQuestionsCurrentUserByChallengeId, createChallengeQuestion, deleteChallengeQuestion, answerChallengeQuestion } from '../controllers/challengeQuestionController.js';
+import authMiddleware from '../middlewares/authMiddleware.js';
 
 const router = express.Router();
 
-router.route('/challenge-questions').get(getAllChallengeQuestions);
-router.route('/challenge-question/:id').get(getChallengeQuestionById);
-router.route('/challenge-questions/user').post(getChallengeQuestionsByUserId);
-router.route('/challenge-question/create').post(createChallengeQuestion);
-router.route('/challenge-question/update/:id').put(answerChallengeQuestion);
-router.route('/challenge-question/delete/:id').delete(deleteChallengeQuestion);
+router.route('/challenge-questions').get(authMiddleware, getAllChallengeQuestions);
+router.route('/challenge-question/:id').get(authMiddleware, getChallengeQuestionById);
+router.route('/challenge-questions/user').get(authMiddleware, getChallengeQuestionsCurrentUserByChallengeId);
+router.route('/challenge-question/create').post(authMiddleware, createChallengeQuestion);
+router.route('/challenge-question/answers').put(authMiddleware, answerChallengeQuestion);
+router.route('/challenge-question/delete/:id').delete(authMiddleware, deleteChallengeQuestion);
 
 export default router;
